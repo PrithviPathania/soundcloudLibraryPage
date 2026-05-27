60023c024d36a2a36d041135c3161b0131d2b5fe
PrithviPathania <pathaniaprithvi1@gmail.com>
2026-05-27 03:40:13 -0600

    Completed header
    header is complete consists of all icons that are in the soundcloud mobile app header on the library page, only difference is the "Get Artist Pro" is a gradient in the original app and just a pink bg color in mine, since react doesn't have a built in gradient. I could possibly fix this later using expo linear gradient.

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@ import { Text, View, StyleSheet,Image} from "react-native";
 export default function Index() {
   return (
     <View style={{ flex: 1, backgroundColor: "#282828" }}>
+      
       <View style={styles.header}>
 
       <Text style={styles.getArtistPro}>
@@ -10,10 +11,27 @@ export default function Index() {
       
       <Text style={styles.library}>Library</Text>
 
-      
+
+       <Image
+        source={{
+          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Settings-icon-symbol-vector.png/960px-Settings-icon-symbol-vector.png",
+        }}      
+        style={styles.settingsIcon}
+      />
+        <Image
+        source={{
+          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/VK_icons_profile_28.svg/960px-VK_icons_profile_28.svg.png",
+        }}
+        style={styles.profilePicture}
+      />
 
 
       </View>
+
+      
+    
+      
+
     </View>
   );
 }
@@ -33,6 +51,18 @@ const styles = StyleSheet.create({
   library: {
     color: "#ffffff",
     marginLeft: 70,
-
+    alignItems: "center",
+  },
+  settingsIcon: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginLeft: 80,
+  },
+  profilePicture: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginLeft: 5, 
   },
 });
